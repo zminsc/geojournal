dd7a3c66373ade8c248280fffba94a48ba37e20a
Steven Chang <zminsc@seas.upenn.edu>
2024-12-01 16:50:11 -0500

    Initial commit

---
+++ GeoJournal/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GeoJournal
+//
+//  Created by Steven Chang on 12/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GeoJournal/GeoJournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeoJournalApp.swift
+//  GeoJournal
+//
+//  Created by Steven Chang on 12/1/24.
+//
+
+import SwiftUI
+
+@main
+struct GeoJournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
